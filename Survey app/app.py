5b5d5480f73884377a56a98c8f1d9e9464a66483
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2020-09-18 23:17:41 +0530

    Data load and basic layout update

--- Survey app/app.py
+++ Survey app/app.py
@@ -3,14 +3,27 @@ import dash_core_components as dcc
 import dash_html_components as html 
 from dash.dependencies import Input, Output
 import plotly.express as px
+import data
 
 app = dash.Dash(__name__)
+
+d = data.data
+
+airline = d['airline'].unique()
 
 app.layout = html.Div([
 
     html.H1("Flight Data Dashboard", style={'text-align':'center'}),
 
-    
+    html.H4("Select the Airline"),
+    dcc.Dropdown(
+        id="select-airline",
+        options=[{"label":x, "value":x} for x in airline],
+        value="American Airlines Inc.",
+        style={"width":"40%"},
+        multi=False
+    )
+
 
 ])
 
--- Survey app/data.py
+++ Survey app/data.py
@@ -1,23 +1,30 @@
-# #%%
-# import pandas as pd
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/flightdata.csv')
-# pd.set_option('display.max_columns', 31)
+#%%
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/flightdata.csv')
+pd.set_option('display.max_columns', 31)
 
-# df
-# # %%
-# df.drop(columns=['Unnamed: 0','month', 'day','carrier',
-#        'flight', 'tailnum', 'origin', 'dest','time_hour', 'flightb','airport', 'lat', 'lon', 'alt', 'tz', 'dst', 'tzone'], inplace=True)
-# # %%
-# df.head(100)
-# # %%
+
+# %%
+df.drop(columns=['Unnamed: 0','month', 'day','carrier',
+       'flight', 'tailnum', 'origin', 'dest','time_hour', 'flightb','airport', 'lat', 'lon', 'alt', 'tz', 'dst', 'tzone'], inplace=True)
+# %%
+# df.head(5)
+# %%
 # df.isnull().sum()
-# # %%
+# %%
 # df.isna().sum()
-# # %%
-# import numpy as np
-# cols = ['dep_time','dep_delay','arr_time','arr_delay','air_time','avgdelay']
+# %%
+import numpy as np
+cols = ['dep_time','dep_delay','arr_time','arr_delay','air_time','avgdelay']
 
-# for i in cols:
-#     df[i] = df[i].replace(0, np.nan)
-#     m = float(df[i].mean(skipna=True))
-#     df[i] = df[i].replace(np.nan, m)
+for i in cols:
+    df[i] = df[i].replace(0, np.nan)
+    m = float(df[i].mean(skipna=True))
+    df[i] = df[i].replace(np.nan, m)
+# %%
+data = df.copy()
+# %%
+# data.columns
+# %%
+# a = data[data["airline"] == "American Airlines Inc."]
+# a
